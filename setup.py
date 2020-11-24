59d1b84a4c807c10f61618894e56faf9d1cdbb4f
jelly6489 <rlaalsrlzld@naver.com>
2020-11-24 16:06:10 +0900

    Tema

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name='com_blackTensor',
     version='0.1',
     description='first Python Project',
-    author='KainKim',
-    author_email='staridee@gmail.com',
+    author='jelly6489',
+    author_email='jelly6489@gmail.com',
     url='https://www.python.org/sigs/distutils-sig/',
     packages=setuptools.find_packages(),
     python_requires='>=3.7'
